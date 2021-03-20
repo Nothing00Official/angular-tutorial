62010112200be5d76319c203acf51e8839de1a8c
nothing00official <nothing00@nothmail.it>
2021-03-20 13:11:23 +0100

    lezione 4

--- tutorial/src/app/app.module.ts
+++ tutorial/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import localeIt from '@angular/common/locales/it';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeIt, 'it');
 
 @NgModule({
   declarations: [
@@ -16,7 +20,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'it' }, { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- tutorial/src/app/home/home.component.ts
+++ tutorial/src/app/home/home.component.ts
@@ -1,17 +1,59 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   @Input() user: string;
 
-  constructor() { }
+  today: Date;
+
+  money: number;
+
+  @ViewChild('about', { static: false }) aboutElement: ElementRef;
+
+  show: boolean = false;
+
+  constructor() {
+    this.today = new Date();
+
+    let money1 = 2345345433;
+    let money2 = 343453545;
+
+    this.money = this.sum(money1, money2);
+
+    console.log("costruttore!");
+  }
 
   ngOnInit(): void {
+    console.log("init!")
+  }
+
+  sum(n1: number, n2: number): number {
+    return n1 + n2;
+  }
+
+  showHidden(): void {
+    this.show = !this.show;
+  }
+
+  printElement(): void {
+    console.log(this.aboutElement.nativeElement);
+  }
+
+  hello(field: HTMLSelectElement) {
+    if (field.value == "1") {
+      alert("Ciao!")
+    } else {
+      console.log("Ciao!");
+    }
+  }
+
+  ngAfterViewInit(): void {
+    document.getElementById("button").style.backgroundColor = "red";
   }
 
 }
